a01875008628b136ee4e9cc9b20cb04d591d9256
Alex Beeson <beeson.alexander@gmail.com>
2019-10-15 13:37:39 -0400

    back to relative path

--- routes/shopify-routes.js
+++ routes/shopify-routes.js
@@ -110,7 +110,7 @@ router.get('/', (req, res) => {
 
   router.get('/proxy', (req, res) => {
 
-    res.set('Content-Type', 'application/liquid').sendFile(path.join(__dirname, 'app/client/build/index.html'));
+    res.set('Content-Type', 'application/liquid').sendFile(path.join(__dirname, '../client/build/index.html'));
 
   });
 
