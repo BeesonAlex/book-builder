7ba59e4533268d301e0813c8ff634d9a4e99e844
Alex Beeson <beeson.alexander@gmail.com>
2019-11-04 10:26:26 -0500

    adjusting id to props.id

--- client/src/components/splash/BookPreview.js
+++ client/src/components/splash/BookPreview.js
@@ -4,10 +4,10 @@ export class BookPreview extends Component {
 
     render() {
         return (
-            <div className="bookpreview__wrapper" onclick={this.onClickHandler}>
+            <div className="bookpreview__wrapper">
                 <h3 className="bookpreview__title">{this.props.title}</h3>
                 <p className="bookpreview__pages">{this.props.numPage}</p>
-                <button className="bookpreview__edit-button" id={this.props.id} onClick={() => this.props.onBookPreviewClick(this.id)}>Edit</button>
+                <button className="bookpreview__edit-button" id={this.props.id} onClick={() => this.props.onBookPreviewClick(this.props.id)}>Edit</button>
             </div>
         )
     }
