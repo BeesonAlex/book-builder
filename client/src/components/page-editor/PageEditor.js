a9683e0b7fad82ca95d79327a53ab6be676ecc4c
Alex Beeson <beeson.alexander@gmail.com>
2019-11-08 11:53:00 -0500

    switched to defaultvalue method of handling textarea

--- client/src/components/page-editor/PageEditor.js
+++ client/src/components/page-editor/PageEditor.js
@@ -98,22 +98,6 @@ onSubmitHandler = event => {
     });
 }
 
-textChangeHandler = event => {
-    this.setState({
-        activePage: {
-            id: this.state.activePage.id,
-            pageNumber: this.state.activeBook.pages.length + 1,
-            track: `${this.state.trackDetails.name}`,
-            album: `${this.state.trackDetails.album}`,
-            albumArt: `${this.state.trackDetails.albumArt}`,
-            artist: `${this.state.trackDetails.artist}`,
-            thoughts: event.target.thoughts.value,
-            pageContentUrl: this.state.activePage.pageContentUrl,
-        }
-    })
-}
-
-
     render() {
         console.log(this.state)
         if (this.state.trackLoading) {
@@ -139,7 +123,7 @@ textChangeHandler = event => {
                         <h1 className="pageeditor__song-title">{this.state.trackDetails.name}</h1>
                         <h2 className="pageeditor__artist-title">{this.state.trackDetails.artist}</h2>
                         <h2 className="pageeditor__artist-title">{this.state.trackDetails.album}</h2>
-                        <input className="pageeditor__thoughts" type="text" name="thoughts" placeholder="Write your thoughts..." value={this.state.activePage.thoughts} onChange={this.textChangeHandler}></input>
+                        <input className="pageeditor__thoughts" type="text" name="thoughts" placeholder="Write your thoughts..." defaultValue={this.state.activePage.thoughts}></input>
                     </div>
                     <button type="submit">Save and Close</button>
                     </form>
