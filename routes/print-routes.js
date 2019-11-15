653d342ebb8bf8f1b835b136e47d83f2540b57fc
Alex Beeson <beeson.alexander@gmail.com>
2019-11-15 14:25:13 -0500

    new validation middleware for webhooks

--- routes/print-routes.js
+++ routes/print-routes.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const router = Router();
 const axios = require('axios');
 const crypto = require('crypto');
+const getRawBody = require('raw-body')
 
 // Register Webhook to listen for new orders with custom books
 router.post('/webhook/order', validateWebhook, fetchToken, async (req, res) => {
@@ -69,15 +70,28 @@ router.post('/webhook/order', validateWebhook, fetchToken, async (req, res) => {
   });
   
   function validateWebhook (req, res, next){
-    generated_hash = crypto
-        .createHmac('sha256', process.env.SHOPIFY_WEBHOOK_SECRET)
-        .update(Buffer.from(req.rawbody))
-        .digest('base64');
-    if (generated_hash == req.headers['x-shopify-hmac-sha256']) {
-        next()
-    } else {
-        res.sendStatus(403)
-    }
+    // We'll compare the hmac to our own hash
+  const hmac = req.get('X-Shopify-Hmac-Sha256')
+
+  // Use raw-body to get the body (buffer)
+  const body = await getRawBody(req)
+
+  // Create a hash using the body and our key
+  const hash = crypto
+    .createHmac('sha256', secretKey)
+    .update(body, 'utf8', 'hex')
+    .digest('base64')
+
+  // Compare our hash to Shopify's hash
+  if (hash === hmac) {
+    // It's a match! All good
+    console.log('Phew, it came from Shopify!')
+    res.sendStatus(200)
+  } else {
+    // No match! This request didn't originate from Shopify
+    console.log('Danger! Not from Shopify!')
+    res.sendStatus(403)
+  }
   }
 
 
@@ -103,6 +117,7 @@ router.post('/webhook/order', validateWebhook, fetchToken, async (req, res) => {
   let token = await response.data
 
   if (token) {
+    console.log('successfully fetched token')
   res.token = token
   next()
   } else {
@@ -111,3 +126,19 @@ router.post('/webhook/order', validateWebhook, fetchToken, async (req, res) => {
 }
 
   module.exports = router;
+
+
+
+
+//   function validateWebhook (req, res, next){
+//     generated_hash = crypto
+//         .createHmac('sha256', process.env.SHOPIFY_WEBHOOK_SECRET)
+//         .update(Buffer.from(req.rawbody))
+//         .digest('base64');
+//     if (generated_hash == req.headers['x-shopify-hmac-sha256']) {
+//         console.log('successfully validated webhook')
+//         next()
+//     } else {
+//         res.sendStatus(403)
+//     }
+//   }
