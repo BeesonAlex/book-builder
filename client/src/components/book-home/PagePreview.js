3dab8ba2f42affc2a11c235160e00fd3629323e0
Alex Beeson <beeson.alexander@gmail.com>
2019-11-21 10:26:10 -0500

    updating styles in bookhome

--- client/src/components/book-home/PagePreview.js
+++ client/src/components/book-home/PagePreview.js
@@ -6,15 +6,15 @@ export class PagePreview extends Component {
     render() {
         return (
             <div className="pagepreview">
-            <h3 className="pagepreview__song-title">{this.props.track}</h3>
-            <p className="pagepreview__song-artist">{this.props.artist}</p>    
             <p className="pagepreview__page-number">{this.props.pageNumber}</p>
-            <button onClick={() => this.props.onPagePreviewClick(this.props.id)} className="pagepreview__edit-button btn--secondary-accent" id={this.props.id}><img className="icon" src="https://cdn.shopify.com/s/files/1/0262/0584/7649/files/edit.svg?58" />Edit</button>
+            <h3 className="pagepreview__song-title">{this.props.track}</h3>
+            <h3 className="pagepreview__song-artist">{this.props.artist}</h3>    
             {
                 this.props.thoughts ?
                 <img className="pagepreview__complete" src="https://cdn.shopify.com/s/files/1/0262/0584/7649/files/check-circle.svg?58" />
                 : <img className="pagepreview__incomplete" src="https://cdn.shopify.com/s/files/1/0262/0584/7649/files/circle.svg?58" />    
             }    
+            <button onClick={() => this.props.onPagePreviewClick(this.props.id)} className="pagepreview__edit-button btn--secondary-accent" id={this.props.id}><img className="icon" src="https://cdn.shopify.com/s/files/1/0262/0584/7649/files/edit.svg?58" />Edit</button>
             </div>
         )
     }
